b6804143ee5f8e82d2e648b70bc0c9419f52d657
Nate Buwalda <nabuwalda@dmacc.edu>
2019-04-15 17:34:26 -0500

    Controller and submission

---
+++ src/edu/dmacc/codedsm/mvcfizzbuzz/SubmissionControllerImpl.java
@@ -0,0 +1,8 @@
+package edu.dmacc.codedsm.mvcfizzbuzz;
+
+public class SubmissionControllerImpl implements SubmissionController {
+    @Override
+    public void submit(Submission submission) {
+        
+    }
+}
