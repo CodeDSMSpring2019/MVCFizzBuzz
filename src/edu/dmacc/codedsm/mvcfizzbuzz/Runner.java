24a3e7959427532f5b8b6348c2b08bdadb6cebc8
Nate Buwalda <nabuwalda@dmacc.edu>
2019-04-15 16:43:10 -0500

    Runner

---
+++ src/edu/dmacc/codedsm/mvcfizzbuzz/Runner.java
@@ -0,0 +1,17 @@
+package edu.dmacc.codedsm.mvcfizzbuzz;
+
+import java.util.Scanner;
+
+public class Runner {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter a username:");
+        String userName = in.next();
+        System.out.println("Enter a number:");
+        Integer inputNumber = in.nextInt();
+
+    }
+
+}
